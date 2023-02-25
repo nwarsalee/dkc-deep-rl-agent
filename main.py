b2136c0dcf30e93299b777e0d8b5c974225d2324
Nabeel W <nabeel.warsalee@gmail.com>
2023-02-25 16:35:57 -0500

    Create agent and model and setup training code

--- main.py
+++ main.py
@@ -4,11 +4,16 @@
 import retro
 import cv2
 import numpy as np
+# Tensorflow/Keras Imports
 from keras import Sequential
 from keras.layers import Dense, Flatten, Convolution2D
 from keras.optimizers import Adam
+# Reinforcement learning imports
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 
-# Function to build a keras model for DeepQLearning
+# Function to build a keras NeuralNetwork model for DeepQLearning
 def build_model(height, width, channels, actions):
     model = Sequential()
     # Setup convolutional layers
@@ -26,48 +31,86 @@ def build_model(height, width, channels, actions):
 
     return model
 
+# Function to build a DeepQ Learning agent based on the keras AI model
+def build_agent(model, actions):
+    policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
+    memory = SequentialMemory(limit=1000, window_length=3)
+    
+    dqn = DQNAgent(model=model, memory=memory, policy=policy,
+                  enable_dueling_network=True, dueling_type='avg', 
+                   nb_actions=actions, nb_steps_warmup=1000)
+
+    return dqn
+
+# Function to test the SNES gym-retro environment
+def test_gymretro(env):
+    # Iterate over 5 episodes
+    for i in range(5):
+        state = env.reset()
+        score = 0
+        done = False
+
+        # Grab resolution of game image
+        inx, iny, inc = env.observation_space.shape
+        pos = np.array([int(inx/8), int(iny/8)])
+
+        print(pos)
+
+        # Declare vars for x coord
+        xpos = 0
+        xpos_max = 0
+
+        # Game Render loop
+        while not done:
+            # Display what is happening
+            env.render()
+            
+            # Specify action/buttons randomly
+            action = env.action_space.sample()
+
+            # Turn image to grayscale
+            state = cv2.resize(state, (pos[0], pos[1]))
+            state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+            state = np.reshape(state, (pos[0], pos[1]))
+
+            imgArray = state.flatten()
+
+            # Update next frame with current actions
+            state, reward, done, info = env.step(action)
+            
+            # Update score
+            score += reward
+
+            print("Ep#", i, " Action:", action, " | Reward:", reward)
+
+            imgArray = []
+
+    env.close()
+
+
+print("Finished loading in packages...")
+
 # Create new env with gym retro
 env = retro.make("DonkeyKongCountry-Snes", '1Player.CongoJungle.JungleHijinks.Level1')
+# env = retro.make(game='Airstriker-Genesis')
 
-# Iterate over 5 episodes
-for i in range(5):
-    state = env.reset()
-    score = 0
-    done = False
+test = False
 
-    # Grab resolution of game image
-    inx, iny, inc = env.observation_space.shape
-    pos = np.array([int(inx/8), int(iny/8)])
+if test:
+    print(env.action_space)
+    exit()
+    test_gymretro(env)
+else:
+    # Get screen and move information from the environment
+    height, width, channels = env.observation_space.shape
+    actions = env.action_space.n
 
-    print(pos)
+    # Build the model and the agent
+    model = build_model(height, width, channels, actions)
+    print(model.output.shape)
+    agent = build_agent(model, actions)
 
-    # Declare vars for x coord
-    xpos = 0
-    xpos_max = 0
-
-    # Game Render loop
-    while not done:
-        # Display what is happening
-        env.render()
-        
-        # Specify action/buttons randomly
-        action = env.action_space.sample()
-
-        # Turn image to grayscale
-        state = cv2.resize(state, (pos[0], pos[1]))
-        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
-        state = np.reshape(state, (pos[0], pos[1]))
-
-        imgArray = state.flatten()
-
-        # Update next frame with current actions
-        state, reward, done, info = env.step(action)
-        
-        # Update score
-        score += reward
-
-        print("Ep#", i, " Action:", action, " | Reward:", reward)
-
-        imgArray = []
-
-env.close()
+    # Compile model and run training
+    agent.compile(Adam(lr=1e-4))
+    #TODO: Fix actions not being subscriptable...
+    agent.fit(env, nb_steps=1000, visualize=True, verbose=2)
